fc7126e8350f7fe12c4ecdfd2eac2f8848e22566
Andrei Marcu <@marcuandrei1>
2025-10-11 21:29:04 +0300

    firsst commit

---
+++ MoneyManagementApp_backEnd/src/main/java/org/example/moneymanagementapp/MoneyManagementAppApplication.java
@@ -0,0 +1,24 @@
+package org.example.moneymanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController // ca sa ne apara pe web trebuie neaparat sa avem marcat cu @RestController, altfel serverul nu stie
+public class MoneyManagementAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MoneyManagementAppApplication.class, args);
+
+    }
+
+    @CrossOrigin
+    @GetMapping("/home")
+    public String home(){
+        return "hehehe bboy";
+    }
+}
---
+++ MoneyManagementApp_backEnd/src/test/java/org/example/moneymanagementapp/MoneyManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moneymanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
