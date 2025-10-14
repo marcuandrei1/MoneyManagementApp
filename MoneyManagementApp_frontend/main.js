61d15b8821258db77ba5b2a862533a580d20d0b8
Andrei Marcu <@marcuandrei1>
2025-10-14 14:49:56 +0300

    doar main branch update

---
+++ MoneyManagementApp_frontend/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // optional
+    },
+  });
+
+  // ✅ Load your React app (from the dev server or from build)
+  win.loadURL('http://localhost:3000'); // React dev server
+   //win.loadFile(path.join(__dirname, 'build', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
